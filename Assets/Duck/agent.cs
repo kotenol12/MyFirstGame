b902b3d53803af1b97727522593d98a7dfe0ed7d
kotenol12 <mr.climz1@mail.ru>
2021-03-21 22:40:57 +0300

    update agent.cs

--- Assets/Duck/agent.cs
+++ Assets/Duck/agent.cs
@@ -8,8 +8,7 @@ public class agent : MonoBehaviour
     public GameObject target;
     void Update()
     {
-        transform.LookAt(target.transform.position); 
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime); 
+     
     }
 }
 
